67b0bd0653d2afa9727dced712dada0c4f146544
dtaldeja <dtaldeja@LAP-DTALDEJA.elogex.com>
2019-07-23 19:42:39 +0530

    submitted

--- js/login.js
+++ js/login.js
@@ -1,31 +1,50 @@
 
+Ext.QuickTips.init();
+Ext.form.Field.prototype.msgTarget ='side';
+
 Ext.onReady(function() {
 
-var myform = new Ext.form.FormPanel({
-width:400,
+
+var myform = new Ext.FormPanel({
+width:800,
 height:250,
-renderTo:document.body,
+defaultType:'textfield',
 items:[{
 xtype: 'textfield'
-,fieldlabel: 'User Name'
+,fieldLabel: 'User Name'
 ,inputTpe: 'password'
-,width: 150`````
+,width: 250
 ,id: 'txtuserName'
 ,allowBlank:false
+,blankText:'UserName Field is Mandatory'
 ,minLength:3
 ,maxLength: 30
 },{
 xtype: 'textfield'
-,fieldlabel: 'Password'
+,fieldLabel: 'Password'
 ,id:'txtPassword'
 ,inputTpe: 'password'
-,width: 150
+,width: 250
 ,allowBlank:false
+,blankText:'Password Field is Mandatory'
 ,minLength:3
 ,maxLength: 30
 }]
 
 });
 
-)}
+var panel = new Ext.Panel({
+width:800,
+height:250,
+renderTo:document.body,
+buttonAlign: 'center',
+buttons:[{text:'Login'},{text:'Reset'}],
+title: 'Welcome to ONE NETWORK',
+frame:true,
+layout : 'form',
+items : [myform]
+});
+
+
+})
 
